c60ad883f090e64ae920bff511159f8e9d2fa819
Rafael Missio <rtmissio@gmail.com>
2021-07-24 23:17:02 -0300

    adding hooks

--- src/test/java/steps/InserirContasSteps.java
+++ src/test/java/steps/InserirContasSteps.java
@@ -1,5 +1,6 @@
 package steps;
 
+import io.cucumber.java.After;
 import io.cucumber.java.pt.Dado;
 import io.cucumber.java.pt.Então;
 import io.cucumber.java.pt.Quando;
@@ -54,19 +55,21 @@ public class InserirContasSteps {
     public void aContaÉInseridaComSucesso() {
         String texto =  driver.findElement(By.xpath("//div[@class='alert alert-success']")).getText();
         Assert.assertEquals("Conta adicionada com sucesso!", texto);
-        driver.quit();
     }
     @Então("sou notificar que o nome da conta é obrigatório")
     public void souNotificarQueONomeDaContaEObrigatório() {
         String texto =  driver.findElement(By.xpath("//div[@class='alert alert-danger']")).getText();
         Assert.assertEquals("Informe o nome da conta", texto);
-        driver.quit();
     }
 
     @Então("sou notificado que já existe uma conta com esse nome")
     public void souNotificadoQueJaExisteUmaContaComEsseNome() {
         String texto =  driver.findElement(By.xpath("//div[@class='alert alert-danger']")).getText();
         Assert.assertEquals("Já existe uma conta com esse nome!", texto);
+    }
+
+    @After
+    public void fechandoOBrowser(){
         driver.quit();
     }
 }
